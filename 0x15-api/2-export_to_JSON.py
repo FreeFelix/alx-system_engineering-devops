6ab43a0fedf59e07e2d0b949ef6e934d0f955a0f
FreeFelix <nijeanfelix@gmail.com>
2024-02-20 01:33:22 +0200

    New add

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,18 +3,14 @@
 import json
 import requests
 import sys
-
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    
     # Fetch user data based on provided employee ID
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    
     # Fetch TODO list based on provided employee ID
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
     # Write data to JSON file
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,13 +2,10 @@
 """Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    
     # Fetch all users
     users = requests.get(url + "users").json()
-
     # Write data to JSON file
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
