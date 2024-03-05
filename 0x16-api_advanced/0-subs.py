43165f6a58a6dbe04b5a4e3ab9552622da8fb752
FreeFelix <nijeanfelix@gmail.com>
2024-03-05 18:15:17 +0200

    Inintial commit for api advanced task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    A function that queries the Reddit API and returns the
+    number of subscribers (not active users, total subscribers) for
+    a given subreddit. If an invalid subreddit is given, the function should return 0.
+
+    Hint: No authentication is necessary for most features of the Reddit API.
+    If you’re getting errors related to Too Many Requests, ensure you’re setting
+    a custom User-Agent.
+
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
