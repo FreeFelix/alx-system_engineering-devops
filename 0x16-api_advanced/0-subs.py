5a3cde6b0591ca46e075fd12a4ba106fe9ea8679
FreeFelix <nijeanfelix@gmail.com>
2024-03-07 23:09:06 +0200

    New commit to debug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
 """
-A function that queries the Reddit API and returns the
-number of subscribers (not active users, total subscribers) for
-a given subreddit. If an invalid subreddit is given, the function should return 0.
-
-Hint: No authentication is necessary for most features of the Reddit API.
-If you’re getting errors related to Too Many Requests, ensure you’re setting
-a custom User-Agent.
-"""
-
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
-
-    Args:
-    subreddit (str): The name of the subreddit.
-
-    Returns:
-    int: The total number of subscribers.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    r = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if r.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    json = r.json()
+    data_dict = json.get('data')
+    return(data_dict.get('subscribers'))
